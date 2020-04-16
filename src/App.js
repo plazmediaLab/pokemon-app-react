3311e7f0ae149c1531350f143a36e0fff736bbe6
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-04-16 16:48:25 -0500

    Gradiant effect add, with :hover CSS function

--- src/App.js
+++ src/App.js
@@ -1,43 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import logo from './img/logo.svg';
+import { Global, css } from '@emotion/core';
 // Styled components
 import { Main, Palette, ActionButton } from './components/StyledComponents';
 
 function App() {
 
   // Local STATE
-  const [colors, setColors] = useState({
+  const [ac, setAC] = useState({
     primary: '#efe782',
     secondary: '#de782e',
     tertiary: '#d53d30',
   });
-  const { primary, secondary, tertiary } = colors; 
+  const [bc, setBC] = useState({
+    primary: '#90cfb3',
+    secondary: '#327258',
+    tertiary: '#b64455',
+  });
 
-  console.log(colors);
-
-  const onBlusrEvent = () => {
-    console.log('Item hover');
+  const onClickFunc = () => {
+    setAC({
+      primary: '#90cfb3',
+      secondary: '#327258',
+      tertiary: '#b64455',
+    })
   };
 
   return (
+    // <Main theme={color}>
+    <Fragment>
+      <Global 
+        styles={css`
+          :root{
+            --palette-primary: ${ac.primary};
+            --palette-secondary: ${ac.secondary};
+            --palette-tertiary: ${ac.tertiary};
+          }
+          .main:hover .primary,
+          .main:hover .secondary,
+          .main:hover .tertiary,
+          .main:hover .gradiant{
+            --palette-primary: ${bc.primary};
+            --palette-secondary: ${bc.secondary};
+            --palette-tertiary: ${bc.tertiary};
+          }
+          .main:hover .gradiant{
+            opacity: 1;
+          }
+        `}
+      />
 
-      // <Main theme={color}>
-      <Main 
-        colors={colors}
-        onMouseOver={onBlusrEvent}
-      >
+      <Main className="main">
 
-        <Palette
-          colors={colors}
-        >
+        <div className='gradiant'></div>
+
+        <Palette>
           <div className="primary"></div>
           <div className="secondary"></div>
           <div className="tertiary"></div>
         </Palette>
 
-        <ActionButton>Vamo'a calmarno</ActionButton>
+        <ActionButton
+          onClick={onClickFunc}
+        >Vamo'a calmarno</ActionButton>
 
       </Main>
+    </Fragment>
 
   );
 }
--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -1,32 +1,45 @@
 import styled from '@emotion/styled';
 
 export const Main = styled.div`
+  position: relative;
+  z-index: 20;
   height: 100vh;
   display: grid;
   grid-template-columns: 1fr; 
   grid-template-rows: 100px 1fr;
   align-items: center;
   justify-items: center; 
-
-  /* --primary: #efe782;
-  --secondary: #de782e;
-  --tertiary: #d53d30; */
+  background: 
+    linear-gradient(var(--palette-primary), transparent),
+    linear-gradient(90deg, var(--palette-secondary), transparent),
+    linear-gradient(-90deg, var(--palette-tertiary), transparent);
 
   /* background-color: ${props => props.theme}; */
-  background: 
-    linear-gradient(${props => props.colors.primary}, transparent),
-    linear-gradient(90deg, ${props => props.colors.secondary}, transparent),
-    linear-gradient(-90deg, ${props => props.colors.tertiary}, transparent);
-  transition: 
-    ${props => props.colors.primary} 1s,
-    ${props => props.colors.secondary} 1s,
-    ${props => props.colors.tertiary} 1s;
 
-    :hover{
-      --primary: #90cfb3;
-      --secondary: #327258;
-      --tertiary: #b64455;
-    }
+  /* &:hover{
+    --palette-primary: #90cfb3;
+    --palette-secondary: #327258;
+    --palette-tertiary: #b64455;
+  } */
+  .gradiant{
+    position: absolute;
+    z-index: -1;
+    content: '';
+    top: 0;
+    left: 0;
+    background: 
+      linear-gradient(var(--palette-primary), transparent),
+      linear-gradient(90deg, var(--palette-secondary), transparent),
+      linear-gradient(-90deg, var(--palette-tertiary), transparent);
+    width: 100vw;
+    height: 100vh;
+    opacity: 0;
+    transition: opacity 1s ease-in-out;
+    /* display: block; */
+  }
+  /* &:hover::before{
+    opacity: 1;
+  } */
 `;
 
 export const Palette = styled.div`
@@ -36,27 +49,28 @@ export const Palette = styled.div`
   justify-self: stretch;
 
   .primary{
-    background-color: ${props => props.colors.primary};
+    background: var(--palette-primary);
+    transition: background .5s ease-in-out;
   }
   .secondary{
-    background-color: ${props => props.colors.secondary};
+    background: var(--palette-secondary);
+    transition: background .5s ease-in-out;
   }
   .tertiary{
-    background-color: ${props => props.colors.tertiary};
+    background: var(--palette-tertiary);
+    transition: background .5s ease-in-out;
   }
 `;
 
 export const ActionButton = styled.button`
-  position: relative;
   padding: 1em 2em;
   font-size: 2rem;
   border: none; 
   clip-path: 
     polygon(10% 0, 100% 0, 100% 0, 100% 65%, 90% 100%, 0 100%, 0 100%, 0 35%);
   overflow: visible;
-  z-index: 20;
 
-  :hover{
+  &:hover{
     background: #e6e6e6;
     box-shadow: 0px 7px 7px -5px rgba(0,0,0,0.3)!important;
   }
